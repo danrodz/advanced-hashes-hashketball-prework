bbe4a787686817f6a6648d44fc862c8db71d212b
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-27 03:10:07 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -145,12 +145,13 @@ end
 
 def team_colors(team_name)
   hash = game_hash
-  colors = nil
+  colors = []
   hash.each do |team, statistics|
     if team == team_name
-      colors = hash[team][:colors] 
+      colors += hash[team][:colors] 
     end
   end
+  colors
 end
 
 
